7d78342669d579685b0fb4ab6e09842759b5a25d
ayo <ayopro89@gmail.com>
2023-08-18 13:04:42 +0100

    add sudoku controllers

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -1,24 +1,64 @@
 class SudokuSolver {
-
   validate(puzzleString) {
+    const regexPuzzle = /^[1-9.]+$/;
+
+    if (puzzleString.length < 81) {
+      return { valid: false, msg: "Expected puzzle to be 81 characters long" };
+    }
+
+    else if (!regexPuzzle.test(puzzleString)) {
+      console.log(puzzleString)
+      return { valid: false, msg: "Invalid characters in puzzle" };
+    }
+    else if (puzzleString === "" || puzzleString === undefined)
+      return { valid: false, msg: "Required field missing" };
+
+    let board = [];
+
+    for (let i = 0; i < 9; i++) {
+      board[i] = puzzleString.slice(i * 9, (i + 1) * 9).split("");
+    }
+
+    for (let i = 0; i < 9; i++) {
+      for (let j = 0; j < 9; j++) {
+        let value = board[i][j];
+        if (value !== ".") {
+          board[i][j] = ".";
+          if (
+            !this.checkRowPlacement(board, i, j, value) ||
+            !this.checkColPlacement(board, i, j, value) ||
+            !this.checkRegionPlacement(board, i, j, value)
+          ) {
+            return "Invalid puzzle";
+          }
+          board[i][j] = value;
+        }
+      }
+    }
+    return { valid: true, board: board };
   }
 
   checkRowPlacement(puzzleString, row, column, value) {
-
+    for (let i = 0; i < 9; i++) {
+      if (puzzleString[row][i] === value) {
+        return false;
+      }
+    }
+    return true;
   }
 
   checkColPlacement(puzzleString, row, column, value) {
-
+    for (let i = 0; i < 9; i++) {
+      if (puzzleString[i][column] === value) {
+        return false;
+      }
+    }
+    return true;
   }
 
-  checkRegionPlacement(puzzleString, row, column, value) {
+  checkRegionPlacement(puzzleString, row, column, value) {}
 
-  }
-
-  solve(puzzleString) {
-    
-  }
+  solve(puzzleString) {}
 }
 
 module.exports = SudokuSolver;
-
--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -1,9 +1,35 @@
-const chai = require('chai');
+const chai = require("chai");
 const assert = chai.assert;
 
-const Solver = require('../controllers/sudoku-solver.js');
-let solver;
+const Solver = require("../controllers/sudoku-solver.js");
+let solver = new Solver();
 
-suite('Unit Tests', () => {
+suite("Unit Tests", () => {
+  suite("Sudoku Solver Tests", () => {
+    suite("Function validate(puzzleString)", () => {
+      test("should return invalid when puzzleString is not 81 characters", () => {
+        const invalidPuzzle =
+          "1..2..3..4..5..6..7..8..9..1..2..3..4..5..6..7..8";
+        const result = solver.validate(invalidPuzzle);
+        console.log(result);
+        assert.equal(result.valid, false);
+        assert.equal(result.msg, "Expected puzzle to be 81 characters long");
+      });
 
+      test("should return invalid when puzzleString contains invalid characters", () => {
+        const invalidPuzzle = "1..2..3..4..5..6..7..8..9.1..2..3..4..5..6..7..8..9..a..2..3..4..5";
+        const result = solver.validate(invalidPuzzle);
+        assert.equal(result.valid, false);
+        assert.equal(result.msg, "Invalid characters in puzzle");
+      });
+
+      test("should return valid when puzzleString is a valid puzzle", () => {
+        const validPuzzle =
+          "5.9..6..3.8..2..7....3..4.....1.7.....5.6.....8.2.....4..9....5..3..2.1..4..6.7";
+        const result = solver.validate(validPuzzle);
+        result.should.have.property("valid").equal(true);
+      });
+    });
+  });
+  //Luego de tener el código de prueba listo, ejecuta las pruebas utilizando el comando mocha en la línea de comandos. Si todo está configurado correctamente, verás los resultados de las pruebas en la terminal.
 });
